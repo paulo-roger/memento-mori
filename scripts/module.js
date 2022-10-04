4a02d3360d84803518b36b269519c68992933232
Paulo Roger <me@pauloroger.dev>
2022-10-03 22:52:05 -0300

    updated to pf2e

--- scripts/module.js
+++ scripts/module.js
@@ -1,5 +1,5 @@
 //Dev mode integration
-const MODULE_ID = "memento-mori";
+const MODULE_ID = "memento-mori-pf2e";
 
 Hooks.once('devModeReady', ({ registerPackageDebugFlag }) => {
     registerPackageDebugFlag(MODULE_ID);
@@ -12,17 +12,17 @@ function log(...args) {
         if (isDebugging) {
             console.log(MODULE_ID, '|', ...args);
         }
-    } catch (e) {}
+    } catch (e) { }
 }
 
 //settings
-function getSetting(key){
+function getSetting(key) {
     return game.settings.get(MODULE_ID, key)
 }
 
-Hooks.once('init', async function() {
+Hooks.once('init', async function () {
 
-    game.settings.register(MODULE_ID, "unlinkedStatusName",{
+    game.settings.register(MODULE_ID, "unlinkedStatusName", {
         name: "MEMENTO_MORI.Settings.UnlinkedStatusName.Name",
         hint: "MEMENTO_MORI.Settings.UnlinkedStatusName.Hint",
         scope: 'world',     // "world" = sync to db, "client" = local storage
@@ -31,7 +31,7 @@ Hooks.once('init', async function() {
         default: "Dead"
     })
 
-    game.settings.register(MODULE_ID, "unlinkedStatusIcon",{
+    game.settings.register(MODULE_ID, "unlinkedStatusIcon", {
         name: "MEMENTO_MORI.Settings.UnlinkedStatusIcon.Name",
         hint: "MEMENTO_MORI.Settings.UnlinkedStatusIcon.Hint",
         scope: 'world',     // "world" = sync to db, "client" = local storage
@@ -41,7 +41,7 @@ Hooks.once('init', async function() {
         default: `modules/${MODULE_ID}/icons/pirate-grave.svg`
     })
 
-    game.settings.register(MODULE_ID, "linkedStatusName",{
+    game.settings.register(MODULE_ID, "linkedStatusName", {
         name: "MEMENTO_MORI.Settings.LinkedStatusName.Name",
         hint: "MEMENTO_MORI.Settings.LinkedStatusName.Hint",
         scope: 'world',     // "world" = sync to db, "client" = local storage
@@ -50,7 +50,7 @@ Hooks.once('init', async function() {
         default: "Dying"
     })
 
-    game.settings.register(MODULE_ID, "linkedStatusIcon",{
+    game.settings.register(MODULE_ID, "linkedStatusIcon", {
         name: "MEMENTO_MORI.Settings.LinkedStatusIcon.Name",
         hint: "MEMENTO_MORI.Settings.LinkedStatusIcon.Hint",
         scope: 'world',     // "world" = sync to db, "client" = local storage
@@ -60,7 +60,7 @@ Hooks.once('init', async function() {
         default: `modules/${MODULE_ID}/icons/bleeding-wound.svg`
     })
 
-    game.settings.register(MODULE_ID, "overlay",{
+    game.settings.register(MODULE_ID, "overlay", {
         name: "MEMENTO_MORI.Settings.Overlay.Name",
         hint: "MEMENTO_MORI.Settings.Overlay.Hint",
         scope: 'world',     // "world" = sync to db, "client" = local storage
@@ -69,16 +69,16 @@ Hooks.once('init', async function() {
         default: true
     })
 
-    game.settings.register(MODULE_ID, "hitPath",{
+    game.settings.register(MODULE_ID, "hitPath", {
         name: "MEMENTO_MORI.Settings.HitPath.Name",
         hint: "MEMENTO_MORI.Settings.HitPath.Hint",
         scope: 'world',     // "world" = sync to db, "client" = local storage
         config: true,       // false if you dont want it to show in module config
         type: String,       // Number, Boolean, String,
-        default: "data.attributes.hp.value"
+        default: "system.attributes.hp.value"
     })
 
-    game.settings.register(MODULE_ID, "comparison",{
+    game.settings.register(MODULE_ID, "comparison", {
         name: "MEMENTO_MORI.Settings.comparison.Name",
         hint: "MEMENTO_MORI.Settings.comparison.Hint",
         scope: 'world',     // "world" = sync to db, "client" = local storage
@@ -91,7 +91,7 @@ Hooks.once('init', async function() {
         }
     })
 
-    game.settings.register(MODULE_ID, "compareTo",{
+    game.settings.register(MODULE_ID, "compareTo", {
         name: "MEMENTO_MORI.Settings.CompareTo.Name",
         hint: "MEMENTO_MORI.Settings.CompareTo.Hint",
         scope: 'world',     // "world" = sync to db, "client" = local storage
@@ -101,44 +101,49 @@ Hooks.once('init', async function() {
     })
 });
 
-async function addEffect(actor){
+async function addEffect(actor) {
     log("Adding Effect to " + actor.name)
-    if(actor.effects.find(e => e.data?.flags?.core?.statusId === MODULE_ID)) return //no new effect if one is already present
+    if(actor.items.find(e => e.flags?.core?.statusId === MODULE_ID)) return //no new effect if one is already present
+    let token = canvas.scene.tokens.find(t => t.actorId === actor.id)
     let linked = actor.isToken ? "unlinked" : "linked"
-    let label = getSetting(`${linked}StatusName`);
-    let icon = getSetting(`${linked}StatusIcon`);
+    let name = getSetting(`${linked}StatusName`);
+    let img = getSetting(`${linked}StatusIcon`);
     let effect = {
-        label,
-        icon,
+        name,
+        img,
+        type: "effect",
         flags: {
-            core:{
+            core: {
                 statusId: MODULE_ID,
-                overlay: getSetting("overlay")
             }
         }
     }
-    await actor.createEmbeddedDocuments("ActiveEffect", [effect])
+    await actor.createEmbeddedDocuments("Item", [effect])
+    if (getSetting("overlay"))
+        await token.update({overlayEffect: img})
 }
 
-async function removeEffects(actor){
+async function removeEffects(actor) {
     log("Removing Effect From" + actor.name)
-    let effects = actor.effects.filter(e => e.data?.flags?.core?.statusId === MODULE_ID)
-    if (effects.length===0) return
+    let token = canvas.scene.tokens.find(t => t.actorId === actor.id)
+    let effects = actor.items.filter(e => e.flags?.core?.statusId === MODULE_ID)
+    if (effects.length === 0) return
     for (let effect of effects) {
         await effect.delete()
     }
+    await token.update({overlayEffect: ''})
 }
 
-async function updateActor(actor, update){
+async function updateActor(actor, update) {
     if(!game.user === game.users.find(u => u.isGM && u.active)) return //first GM only
     let hp = getProperty(update, getSetting("hitPath"))
-    if(hp === undefined) {
-        if(getProperty(actor.data, getSetting("hitPath"))===undefined) console.warn(`${MODULE_ID} | The setting ${game.i18n.localize("MEMENTO_MORI.Settings.HitPath.Name")} is not a valid property of actor.data or that property is undefined`)
+    if (hp === undefined) {
+        if (getProperty(actor, getSetting("hitPath")) === undefined) console.warn(`${MODULE_ID} | The setting ${game.i18n.localize("MEMENTO_MORI.Settings.HitPath.Name")} is not a valid property of actor or that property is undefined`)
         return
     }
-    let compareTo = isNaN(parseInt(getSetting("compareTo"))) ? getProperty(actor.data, getSetting("compareTo")) : parseInt(getSetting("compareTo"))
+    let compareTo = isNaN(parseInt(getSetting("compareTo"))) ? getProperty(actor, getSetting("compareTo")) : parseInt(getSetting("compareTo"))
     if (compareTo === undefined) {
-        console.warn(`${MODULE_ID} | The setting ${game.i18n.localize("MEMENTO_MORI.Settings.compareTo.Name")} is not a number or a valid property of actor.data or that property is undefined`)
+        console.warn(`${MODULE_ID} | The setting ${game.i18n.localize("MEMENTO_MORI.Settings.compareTo.Name")} is not a number or a valid property of actor or that property is undefined`)
         return
     }
     let comparison = getSetting("comparison")
@@ -151,8 +156,8 @@ async function updateActor(actor, update){
             dead = hp >= compareTo;
             break
     }
-    if(dead) await addEffect(actor)
-    if(!dead && hp) await removeEffects(actor) //only remove if not dead and if hp exists, to avoid false removal
+    if (dead) await addEffect(actor)
+    if (!dead && hp) await removeEffects(actor) //only remove if not dead and if hp exists, to avoid false removal
 }
 Hooks.on("ready", () => {
     if (game.user.isGM) Hooks.on("updateActor", updateActor)
